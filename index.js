43fe629400e1eed2ed0a444c88f5f846bb90713e
fcms14 <felipecms14@hotmail.com>
2022-05-01 19:30:35 -0300

    pullRequest

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const bob = document.querySelector('.face')
-
-let distance = 0
+let distanceVw = 0
+let distanceVh = 0
+const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
+const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
 
 function moveBob() {
-    distance += 50
-    bob.style.transform = `translate(${distance}px)`
+    distanceVw += 50
+
+    if(distanceVw >= (vw - 200)){
+        distanceVh +=200
+        distanceVw = 0
+    }
+
+    bob.style.transform = `translateX(${distanceVw}px)  translateY(${distanceVh}px)`
 }
 
 bob.addEventListener('click', moveBob)
